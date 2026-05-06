88876ec84200861c26942de68676ee21058f92cd
pantelinmisha0414-oss <pantelinmisha0414@gmail.com>
2026-05-06 16:38:34 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+TOKEN = "DISCORD_TOKEN"
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Бот запущен как {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
