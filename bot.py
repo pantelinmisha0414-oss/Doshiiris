cdb5772fafccd4e2bd60c79e10945788f4739420
pantelinmisha0414-oss <pantelinmisha0414@gmail.com>
2026-05-06 17:12:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import discord
+import os
 from discord.ext import commands
 
-TOKEN = "DISCORD_TOKEN"
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
